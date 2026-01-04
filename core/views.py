656ba38f6e4eeae34d0abb75fcaac7aa80a6d8e4
Mhalik <mhalikperez@gmail.com>
2026-01-04 19:43:20 +0800

    Added templates and media folder

--- core/views.py
+++ core/views.py
@@ -5,10 +5,6 @@ from .models import Asset
 def home(request):
     assets = Asset.objects.all()
 
-    content = "<h1> Welcome to Collem! </h1>"
-    content += "<h3> Stored Files: </h3>"
+    context = {'assets': assets}
 
-    for asset in assets:
-        content += f"<p>{asset.file.name}</p>"
-
-    return HttpResponse(content)
+    return render (request, 'core/home.html', context)
